26164a1a845a2ecdcfb239e5c7096b40ebbf6877
Arjun Sreedharan <asreedharan@vmware.com>
2020-10-23 14:58:36 +0000

    Update buildpack.toml fields per new packit

--- integration/simple_app_test.go
+++ integration/simple_app_test.go
@@ -63,7 +63,7 @@ func testSimpleApp(t *testing.T, context spec.G, it spec.S) {
 					settings.Buildpacks.NPMInstall.Online,
 					settings.Buildpacks.NPMStart.Online,
 				).
-				WithNoPull().
+				WithPullPolicy("never").
 				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred(), logs.String())
 
